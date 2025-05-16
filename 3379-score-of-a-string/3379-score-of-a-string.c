e727117736943e9016973e7c133b2610c19cb790
Lettnin <samueltt.dev@gmail.com>
2025-05-15 23:20:51 -0300

    att

--- 3379-score-of-a-string/3379-score-of-a-string.c
+++
@@ -1,7 +0,0 @@
-int scoreOfString(char* string) {
-    int pontuacao = 0;
-    for (int i = 0; string[i+1] != '\0'; i++){
-        pontuacao = pontuacao + abs(string[i] - string[i+1]);
-    }
-    return pontuacao;
-}
